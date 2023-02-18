6d7e3fbbcf948d574b24254cc80458a8039dc8b6
eokks <inboxalin@gmail.com>
2023-02-18 17:52:26 +1200

    2nd player

--- snake/src/snake.java
+++ snake/src/snake.java
@@ -13,63 +13,81 @@ class Coordin {
 }
 
 class Main{
-    public static boolean move(ArrayList<Coordin> coord_sn, int newx, int newy, boolean b){
+    public static boolean move(ArrayList<Coordin> coord_sn, int newx, int newy, boolean b, int field_len, ArrayList<Coordin> coord_sn2){
         for(int i = coord_sn.size()-1;i>0;i--){
             coord_sn.set(i,coord_sn.get(i-1));
         }
-        coord_sn.set(0,new Coordin(newx,newy));
+
+        if(newx > field_len-1 || newx < 0 || newy > field_len-1 || newy < 0 ){
+            b = false;
+        }else{
+            coord_sn.set(0,new Coordin(newx,newy));
+        }
+
         for(int i = 1; i < coord_sn.size();i++){
             if(coord_sn.get(0).x == coord_sn.get(i).x && coord_sn.get(0).y == coord_sn.get(i).y){
                 b = false;
             }
         }
-        if(newx > 9 || newx < 0 || newy > 9 || newy < 0 ){
-            b = false;
+        for(int i = 0; i < coord_sn.size();i++){
+            if(coord_sn.get(0).x == coord_sn2.get(i).x && coord_sn.get(0).y == coord_sn2.get(i).y){
+                b = false;
+            }
         }
+
         return b;
     }
-    public static boolean do_comm(Scanner scan,ArrayList<Coordin> coord_sn, boolean b){
+    public static boolean do_comm(Scanner scan,ArrayList<Coordin> coord_sn, boolean b, int field_len,ArrayList<Coordin> coord_sn2){
         String comm =scan.nextLine();
 
-        int newx = coord_sn.get(0).x;
-        int newy = coord_sn.get(0).y;
-
+        int newx;
+        int newy;
         if(Objects.equals(comm, "w")) {
             newx = coord_sn.get(0).x;
             newy = coord_sn.get(0).y - 1;
+            b = move(coord_sn, newx, newy, b, field_len, coord_sn2);
+
         }else if(Objects.equals(comm, "a")) {
             newx = coord_sn.get(0).x - 1;
             newy = coord_sn.get(0).y;
+            b = move(coord_sn, newx, newy, b, field_len, coord_sn2);
+
         }else if(Objects.equals(comm, "s")) {
             newx = coord_sn.get(0).x;
             newy = coord_sn.get(0).y + 1;
+            b = move(coord_sn, newx, newy, b, field_len, coord_sn2);
+
         }else if(Objects.equals(comm, "d")) {
             newx = coord_sn.get(0).x + 1;
             newy = coord_sn.get(0).y;
+            b = move(coord_sn, newx, newy, b, field_len, coord_sn2);
+
         }else{
             System.out.println("print w, a, s or d");
-            return b;
+            do_comm(scan, coord_sn, b, field_len, coord_sn2);
         }
-
-        b = move(coord_sn, newx, newy, b);
-
         return b;
     }
 
-    public static void ps(ArrayList<Coordin> coord_sn, int field_len){
+    public static void ps(ArrayList<Coordin> coord_sn, int field_len,ArrayList<Coordin> coord_sn2){
+        char[][] map = new char[field_len][field_len];
         for(int y =0; y < field_len; y++){
             for (int x = 0; x < field_len;x++){
-                String a = ".";
-                for (int i = 0; i< coord_sn.size();i++){
-                    if(x ==coord_sn.get(i).x && y == coord_sn.get(i).y){
-                        if(i == 0){
-                            a = "0";
-                        }else{
-                            a = "o";
-                        }
-                    }
-                }
-                System.out.print(a);
+                map[x][y] = '.';
+            }
+        }
+        for(int i =1;i < coord_sn2.size();i++){
+            map[coord_sn2.get(i).x][coord_sn2.get(i).y] = 'x';
+        }
+        map[coord_sn2.get(0).x][coord_sn2.get(0).y] = 'X';
+
+        for(int i =1;i < coord_sn.size();i++){
+            map[coord_sn.get(i).x][coord_sn.get(i).y] = 'o';
+        }
+        map[coord_sn.get(0).x][coord_sn.get(0).y] = 'O';
+        for(int y =0; y < field_len; y++){
+            for (int x = 0; x < field_len;x++){
+                System.out.print(map[x][y]);
             }
             System.out.println("");
         }
@@ -78,16 +96,31 @@ class Main{
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
         int field_len = 10;
-        ArrayList<Coordin> coord_sn = new ArrayList<>();
+        ArrayList<Coordin> coord_sn1 = new ArrayList<>();
         for(int i = 0; i < 5; i++){
-            coord_sn.add(new Coordin(0,0));
+            coord_sn1.add(new Coordin(0,0));
         }
-        boolean b = true;
-        while (b){
+        ArrayList<Coordin> coord_sn2 = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            coord_sn2.add(new Coordin(field_len-1,field_len-1));
+        }
+        boolean b1 = true;
+        boolean b2 = true;
+        while (b1 && b2){
 
-            b = do_comm(scan, coord_sn, b);
-            ps(coord_sn, field_len);
+            ps(coord_sn1, field_len,coord_sn2);
+            System.out.println("Player O's turn. Print w,a,s,d to walk");
+            b1 = do_comm(scan, coord_sn1, b1, field_len,coord_sn2);
+            if(b1){
+                ps(coord_sn1, field_len,coord_sn2);
+                System.out.println("Player X's turn. Print w,a,s,d to walk");
+                b2 = do_comm(scan, coord_sn2, b2, field_len,coord_sn1);
+            }
         }
-        System.out.println("you died");
+        if(b1){
+            System.out.println("Player X died!");
+        }else{
+            System.out.println("Player O died!");
+        }
     }
 }
